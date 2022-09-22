183bafe79b2a33e1344448bc490f066826863718
Nino <ntorres-ulisses@turgot-paris.info>
2022-09-22 17:57:59 +0200

    vue_validerFrais

--- vues/v_validerFrais.php
+++ vues/v_validerFrais.php
@@ -1,4 +1,24 @@
-<?php
-echo "test";
-
-?>
+<fieldset>
+    <legend>Visiteurs et mois à selectionner</legend>
+<label for="lstMois" accesskey="n">Mois : </label>
+        <select id="lstMois" name="lstMois">
+            <?php
+			foreach ($lesMois as $unMois)
+			{
+			    $mois = $unMois['mois'];
+				$numAnnee =  $unMois['numAnnee'];
+				$numMois =  $unMois['numMois'];
+				if($mois == $moisASelectionner){
+				?>
+				<option selected value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option>
+				<?php 
+				}
+				else{ ?>
+				<option value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option>
+				<?php 
+				}
+			
+			}
+           
+		   ?>
+</fieldset>
