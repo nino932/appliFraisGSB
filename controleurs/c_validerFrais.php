49058adedef30451928a18c07848f9437c370137
Nino <ntorres-ulisses@turgot-paris.info>
2022-10-06 17:51:14 +0200

    valider en cours

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -1,16 +1,44 @@
 <?php
 include("vues/v_sommaire.php");
-$action = $_REQUEST['action'];
-$idUtilisateur = $_SESSION['idUtilisateur'];
+$idVisiteur = $_SESSION['idUtilisateur'];
 $mois = getMois(date("d/m/Y"));
-$numAnnee =substr( $mois,0,4);
-$numMois =substr( $mois,4,2);
-switch($action){
-	case 'validerFrais':{
-        $lesVisiteurs=$pdo->getVisiteurs();
-        $lesMois=$pdo->getLesMoisDisponibles('a55');
-		include("vues/v_validerFrais.php");
-	}
-}
+$numAnnee = substr($mois, 0, 4);
+$numMois = substr($mois, 4, 2);
+$action = $_REQUEST['action'];
+
+
 $lesVisiteurs = $pdo->getVisiteurs();
+$lesMois = $pdo->getLesMoisDisponibles('a55');
+include("vues/v_validerFrais.php");
+switch ($action) {
+    case 'voirValiderFrais': {
+            $pdo->getVisiteurs();
+            $lesMois = $pdo->getLesMoisDisponibles('a55');
+            // Afin de sélectionner par défaut le dernier mois dans la zone de liste
+            // on demande toutes les clés, et on prend la première,
+            // les mois étant triés décroissants
+            $lesCles = array_keys($lesMois);
+            $moisASelectionner = $lesCles[0];
+
+            break;
+        }
+    case 'afficherFrais': {
+            $leMois = $_REQUEST['lstMois'];
+            $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
+            $moisASelectionner = $leMois;
+
+            $lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $leMois);
+            $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $leMois);
+            $lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur, $leMois);
+            $numAnnee = substr($leMois, 0, 4);
+            $numMois = substr($leMois, 4, 2);
+            $libEtat = $lesInfosFicheFrais['libEtat'];
+            $montantValide = $lesInfosFicheFrais['montantValide'];
+            $nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
+            $dateModif =  $lesInfosFicheFrais['dateModif'];
+            $dateModif =  dateAnglaisVersFrancais($dateModif);
+            include("vues/v_listeValiderFrais.php");
+        }
+}
+
 ?>
--- vues/v_validerFrais.php
+++ vues/v_validerFrais.php
@@ -1,55 +1,61 @@
 <div id="contenu">
-      <h2>Validation des fiches de frais</h2>
-      <fieldset>
-        <legend><h3>Visiteur et Mois à sélectionner :</h3></legend>
-        <label>Visiteur : </label>
-        <select id="lstVisiteurs" name="lstVisiteurs">
-        <?php
-        foreach ($lesVisiteurs as $unVisiteur) {
-            $visiteurNom = $unVisiteur['nom'];
-            $visiteurPrenom = $unVisiteur['prenom'];
-         ?>
-            <option selected value=><?php echo $visiteurNom . " " . $visiteurPrenom?></option>
-            <?php
-        }
-?>
-</select>
-<br><br>
-      
-        
+    <h3>Validation des fiches de frais : </h3>
 
-        <label for="lstMois" accesskey="n">Mois : </label>
-        <select id="lstMois" name="lstMois">
-            <?php
-			foreach ($lesMois as $unMois)
-			{
-			    $mois = $unMois['mois'];
-				$numAnnee =  $unMois['numAnnee'];
-				$numMois =  $unMois['numMois'];
-				if($mois == $moisASelectionner){
-				?>
-				<option selected value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option>
-				<?php 
-				}
-				else{ ?>
-				<option value="<?php echo $mois ?>"><?php echo  $numMois."/".$numAnnee ?> </option><br>
-				<?php 
-				}
-			
-			}
+    <form action="index.php?uc=validerFrais&action=afficherFrais" method="post">
+        <div class="corpsForm">
 
-            
+            <fieldset>
+                <legend>Visiteurs et mois à sélectionner</legend>
 
-            ?>
+                <p>
+                    <label for="lstVisiteurs" accesskey="n">Visiteur : </label>
+                    <select id="lstVisiteurs" name="lstVisiteurs">
+
+                        <?php
+                        foreach ($lesVisiteurs as $unVisiteur) {
+                            $visiteurNom = $unVisiteur['nom'];
+                            $visiteurPrenom =  $unVisiteur['prenom'];
+                            $visiteurId = $unVisiteur['id'];
+                            if ($mois == $moisASelectionner) {
+                        ?>
+                                <option selected value=""><?php echo $visiteurPrenom . " " . $visiteurNom ?> </option>
+                            <?php
+                            } else { ?>
+                                <option value=""><?php echo $visiteurPrenom . " " . $visiteurNom ?> </option>
+                        <?php
+                            }
+                        }
+                        ?>
+                    </select>
                 </p>
+                <p>
+
+                    <label for="lstMois" accesskey="n">Mois : </label>
+                    <select id="lstMois" name="lstMois">
+                        <?php
+                        foreach ($lesMois as $unMois) {
+                            $mois = $unMois['mois'];
+                            $numAnnee =  $unMois['numAnnee'];
+                            $numMois =  $unMois['numMois'];
+                            if ($mois == $moisASelectionner) {
+                        ?>
+                                <option selected value="<?php echo $mois ?>"><?php echo  $numMois . "/" . $numAnnee ?> </option>
+                            <?php
+                            } else { ?>
+                                <option value="<?php echo $mois ?>"><?php echo  $numMois . "/" . $numAnnee ?> </option>
+                        <?php
+                            }
+                        }
+
+                        ?>
+
+                    </select>
+                </p>
+
+                <div class="piedFormV">
+                    <p>
+                        <input id="ok" type="submit" value="Valider" size="20" />
+                    </p>
+                    
                 </div>
-                <div class="piedForm">
-                <p><br>
-                    <input id="ok" type="submit" value="Valider" size="20" />
-                </p> 
-                </div>
-            </select>
-            
-      </fieldset>
-      
-</div>
+        </div>
